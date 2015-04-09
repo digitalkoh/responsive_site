5aff3ce37a24a38abab9fcf8356ba7e51e1eea4b
Digitalkoh <youngkoh@Young-Koh-mac.local>
2015-04-09 11:10:24 -0500

    Uniden

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
         <div class="footer-bottom">
             <div class="container">
                 <div class="row">
-                    <div class="copyright col-xs-6"> Copyright © My Co </div>
+                    <div class="copyright col-xs-6"> Copyright ©Co </div>
                     <div class="col-xs-6 text-right">
                         <a href="#" class="btn btn-default">&uarr; top</a>
                     </div>
